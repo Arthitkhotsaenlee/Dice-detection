758d443e3524b2a4c54704a521b1f5cd085d51d1
chinton4916@gmail.com <arthit@mindampltd.com>
2023-04-29 18:40:52 +0800

    update with crop image

--- main.py
+++ main.py
@@ -10,14 +10,23 @@ cap = cv2.VideoCapture(0)
 # cap.set(3,640)
 # cap.set(4,640)
 
+size = 640
+
 model = YOLO("runs/detect/train/weights/best.pt")
 classes = ['1','2','3','4','5','6']
-cri_conf = 0.5 # the confidence ceriteria
-
+cri_conf = 0.3 # the confidence ceriteria
+im_x, im_y = 1280, 720
+x_ratio = im_x/size
+y_ratio = im_y/size
 while True:
     suc, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # im_y, im_x = img.shape[0], img.shape[1]
+    # print(im_x, im_y)
+    resize = cv2.resize(img,(size,size),interpolation = cv2.INTER_LINEAR)
+    gray = cv2.cvtColor(resize, cv2.COLOR_BGR2GRAY)
     img_conv = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
+    # img[0:100,0:100,0] = np.zeros((100,100))
+    im_crop = np.array([])
     if suc:
         results = model(img_conv,stream=True)
         dices_val = []
@@ -26,12 +35,16 @@ while True:
             boxes = result.boxes
             for box in boxes:
                 if box and math.ceil((box.conf[0]*1000))/1000 > cri_conf:
+                    # get boundary
+                    x1, y1, x2, y2 = box.xyxy[0] #get boundary
+                    # im_crop = img_conv[int(y1-20):int(y2+20),int(x1-20):int(x2+20),:] # crop images
+                    # get information
                     cls = classes[int(box.cls[0])]
                     dices_val.append(int(cls))
                     conf =  math.ceil((box.conf[0]*1000))/1000
                     total_conf.append(conf)
-                    x1, y1, x2, y2 = box.xyxy[0]
-                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                    # make classification again
+                    x1, y1, x2, y2 = int(x1*x_ratio), int(y1*y_ratio), int(x2*x_ratio), int(y2*y_ratio)
                     # cal with and high
                     w, h = x2-x1, y2-y1 
                     cvzone.cornerRect(img, (x1, y1, w, h))
@@ -51,7 +64,9 @@ while True:
             cvzone.putTextRect(img, str(avg_result),(400,100),)
             cvzone.putTextRect(img, str(dices_val),(400,150),)
         cv2.imshow("Dice-detection", img)
-        # cv2.imshow("conver",img_conv)
+        # if im_crop.any():
+            # cv2.imshow("conver",cv2.resize(im_crop,(size,size)))
+        
         # Waits for a user input to quit the application
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
